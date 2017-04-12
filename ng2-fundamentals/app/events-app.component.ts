5ddadc096f23ca6ffcf2b006836e4b4e702de895
Tuong Nguyen <wallnguyen@gmail.com>
2017-04-12 18:19:50 +0700

    Add events-list.component

--- ng2-fundamentals/app/events-app.component.ts
+++ ng2-fundamentals/app/events-app.component.ts
@@ -6,7 +6,7 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "events-app",
-  template: "<h2>Hello World!</h2>",
+  template: "<events-list></events-list>",
 })
 export class EventsAppComponent {
 
---
+++ ng2-fundamentals/app/events/events-list.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+/**
+ * Created by nctuong on 4/12/2017.
+ */
+@Component({
+  selector: "events-list",
+  template: `
+    <div>
+      <h2>Upcomming Angular 2events</h2>
+    </div>
+  `,
+})
+export class EventsListComponent {
+
+}
