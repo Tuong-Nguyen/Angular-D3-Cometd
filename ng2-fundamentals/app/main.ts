99fa16bcf6ad6971229900382ca5d2e22ea503fc
Tuong Nguyen <wallnguyen@gmail.com>
2017-04-12 16:52:06 +0700

    Add main script and add events-app component into the page

---
+++ ng2-fundamentals/app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by nctuong on 4/12/2017.
+ */
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
