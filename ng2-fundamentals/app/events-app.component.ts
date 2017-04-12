714786c5b79b8575e11e7e2c2b9f13b8d457abd8
Tuong Nguyen <wallnguyen@gmail.com>
2017-04-12 16:50:48 +0700

    Add events-app component which is the main component of the application

---
+++ ng2-fundamentals/app/events-app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by nctuong on 4/12/2017.
+ */
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "events-app",
+  template: "<h2>Hello World!</h2>",
+})
+export class EventsAppComponent {
+
+}
