9846dd49aa8c32fa8c7ad7f0abbdd0d654289ae6
Tuong Nguyen <wallnguyen@gmail.com>
2017-04-14 14:26:59 +0700

    Add event-details.component

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -1,13 +1,15 @@
 /**
  * Created by nctuong on 4/12/2017.
  */
-
-import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {EventDetailsComponent} from "./events/event-details/event-details.component";
 import {EventsAppComponent} from "./events-app.component";
 import {EventThumbnail} from "./events/event-thumbnail.component";
 import {EventsListComponent} from "./events/events-list.component";
 import {EventService} from "./events/shared/event.service";
+import {NgModule} from "@angular/core";
+import {ToastrService} from "./common/toastr.service";
+
 import {NavBarComponent} from "./nav/navbar.component";
 
 @NgModule({
@@ -16,8 +18,13 @@ import {NavBarComponent} from "./nav/navbar.component";
     EventsAppComponent,
     EventsListComponent,
     EventThumbnail,
-  NavBarComponent],
+    NavBarComponent,
+    EventDetailsComponent,
+  ],
   imports: [BrowserModule],
-  providers: [EventService],
+  providers: [
+    EventService,
+    ToastrService,
+  ],
 })
 export class AppModule { }
---
+++ ng2-fundamentals/app/events/event-details/event-details.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+/**
+ * Created by nctuong on 4/14/2017.
+ */
+@Component({
+  styles: [`
+    .container { padding-left: 20px; padding-right: 20px; }
+    .event-image {height: 200px;}
+  `],
+  templateUrl: "app/events/event-details/event-details.component.html",
+})
+export class EventDetailsComponent {}
