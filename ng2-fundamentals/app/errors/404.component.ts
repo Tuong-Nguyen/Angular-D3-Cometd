3836eeb2a381a3f4b381102667defcea2311b87c
Tuong Nguyen <wallnguyen@gmail.com>
2017-04-14 17:03:07 +0700

    Add 404.component

---
+++ ng2-fundamentals/app/errors/404.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+/**
+ * Created by nctuong on 4/14/2017.
+ */
+@Component({
+  templateUrl: 'app/errors/404.component.html',
+  styles: [`
+    .errorMessage {
+      margin-top:150px;
+      font-size: 170px;
+      text-align: center;
+    }
+  `]
+})
+export class Error404Component {}
