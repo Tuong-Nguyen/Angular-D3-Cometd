d11d234ee384b66468fd32344ab5e98c0c89d310
Le Ngoc Thao <lnthao@tma.com.vn>
2017-04-17 18:03:12 +0700

    Create Observable by click event and using XmlHttpRequest to read data

--- RxJSDemo/main.ts
+++ RxJSDemo/main.ts
@@ -2,10 +2,28 @@
  * Created by lnthao on 4/13/2017.
  */
 import {Observable} from "rxjs";
-import {createColdObservable, createHotObservable} from "./hot_and_cold"
 
-/* Cold observable */
-createColdObservable();
+let output = document.getElementById("output");
+let button = document.getElementById("getBtn");
 
-/* Hot observable */
-createHotObservable();
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", ()=> {
+       let movies = JSON.parse(xhr.responseText);
+       movies.forEach(m => {
+           let div = document.createElement("div");
+           div.innerText = m.title;
+           output.appendChild(div);
+       });
+    });
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+click.subscribe(
+    x => load("data/movies.json"),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+)
