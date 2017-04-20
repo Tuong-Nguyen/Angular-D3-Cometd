42caf029db426bc12d00451ef49ba6ab63743b4f
Le Ngoc Thao <lnthao@tma.com.vn>
2017-04-20 11:50:33 +0700

    Use swagger-js-codegen to generate javascript lib from RESTfull API by using swagger documentation

--- RxJSDemo/main.ts
+++ RxJSDemo/main.ts
@@ -1,8 +1,9 @@
 /**
  * Created by lnthao on 4/13/2017.
  */
-import {Observable} from "rxjs";
-import {load} from "./loader"
+import {Observable, Scheduler} from "rxjs";
+import {load} from "./loader";
+import {} from "./petstore";
 
 let output = document.getElementById("output");
 let button = document.getElementById("getBtn");
@@ -26,3 +27,41 @@ click.flatMap(e => merged)
         e => console.log(`error: ${e}`),
         () => console.log("complete")
     );
+
+var observable = Observable.range(1, 10)
+    .scan((acc, x) => { return acc + x; })
+    .observeOn(Scheduler.async);
+
+var observable2 = Observable.range(1, 5)
+    .subscribeOn(Scheduler.asap);
+
+var scheduleObserver = {
+    next: (value) => {
+        Scheduler.async.schedule(
+            (x) => observer.next(x),
+            0 /* delay */,
+            value * 2 /* will be the x for the function above */
+        );
+        console.log(`schedule value: ${value} at ${new Date()}`);
+    },
+    error: (e) => {
+        console.log(`error: ${e}`);
+    },
+
+    complete: () => {
+        console.log(`schedule done`);
+    }
+}
+
+var observer = {
+    next: x => console.log(`value: ${x} at ${new Date()}`),
+    error: err => console.error('something wrong occurred: ' + err),
+    complete: () => console.log('done'),
+};
+// console.log('before subscribe 1');
+// observable.subscribe(scheduleObserver);
+// console.log('after subscribe 1');
+
+console.log('before subscribe 2');
+observable2.subscribe(scheduleObserver);
+console.log('after subscribe 2');
