8e1179c11209cd3ce15f758bfa423e56965e9d28
trinhviethanh2007 <trinhviethanh2007@gmail.com>
2017-04-27 11:21:06 +0700

    remove unused files

--- Oceanalytics/Demo/client/js/factories/fa-socket.js
+++
@@ -1,8 +0,0 @@
-// (function(){
-// 	var client = angular.module('client');
-// 	var socket = function(socketFactory, env){
-// 		var myIoSocket = io.connect(env.SERVER);
-//   		return socketFactory({ ioSocket: myIoSocket });
-// 	}
-// 	client.factory('socket', socket);
-// }());
