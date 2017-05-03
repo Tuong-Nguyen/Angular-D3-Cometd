417cf65444ae2e5fe2bd5d35fba39e80a9e48027
Tuong Nguyen <wallnguyen@gmail.com>
2017-05-03 13:07:03 +0700

    jsf uses promise for generating fake data

--- typescript-fundamental/src/json-schema/GenerateMockData.ts
+++ typescript-fundamental/src/json-schema/GenerateMockData.ts
@@ -3,20 +3,21 @@
  */
 import {schema as jsonSchema} from "./mockDataSchema";
 import fs = require("fs");
-import Chalk = require("chalk");
+import chalk = require("chalk");
 
 declare function require(name: string): any;
 //noinspection TsLint
-var jsf = require("json-schema-faker");
+const jsf = require("json-schema-faker");
 
-const data = JSON.stringify(jsf(jsonSchema));
-
-fs.writeFile("./src/api/db.json", data, (error) => {
-    if (error) {
-        //noinspection TsLint
-        return console.log(Chalk.red(error.message));
-    } else {
-        //noinspection TsLint
-        console.log(Chalk.green("Mock data is generated"));
-    }
+jsf(jsonSchema).then( (sampleData: any) => {
+    fs.writeFile("./src/api/db.json", JSON.stringify(sampleData), (error) => {
+        if (error) {
+            //noinspection TsLint
+            return console.log(chalk.red(error.message));
+        } else {
+            //noinspection TsLint
+            console.log(chalk.green("Mock data is generated"));
+        }
+    });
 });
+
