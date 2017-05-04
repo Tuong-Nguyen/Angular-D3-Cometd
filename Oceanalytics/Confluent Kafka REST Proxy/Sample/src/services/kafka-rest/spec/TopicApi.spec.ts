4b65d7894e917a4ebfc72b6087b72e378fe6ce91
Tuong Nguyen <wallnguyen@gmail.com>
2017-05-04 11:56:58 +0700

    Add 2 tests for TopicApi: GetTopicand produceMessageToTopic

---
+++ Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/kafka-rest/spec/TopicApi.spec.ts
@@ -0,0 +1,48 @@
+/**
+ * Created by nctuong on 5/3/2017.
+ */
+import {TopicApi} from './../api/TopicApi';
+import {HttpModule} from '@angular/http';
+import {async, inject, TestBed} from '@angular/core/testing';
+import {ProduceMessages} from './../model/ProduceMessages';
+import {Record} from './../model/Record';
+
+
+describe('TopicApi', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [TopicApi]
+    }).compileComponents();
+  }));
+
+  describe('#produceMessageToTopic', () => {
+
+    it('send a message return the offset of the message in the topic', async(() => {
+      inject([TopicApi], (topicApi) => {
+        const record: Record = {
+          value: 'This is a message from test'
+        };
+        const records: ProduceMessages = {
+          records: [record]
+        };
+        topicApi.produceMessageToTopic('testTopic', records)
+          .subscribe((result) => {
+            console.log('Hello');
+            expect(result).toBeDefined();
+            expect(result.offsets.length).toBe(1);
+          });
+      })();
+    }));
+  });
+
+  describe('#GetTopic', () => {
+    it('return list topics which has "__consumer_offsets" topic', async(() => {
+      inject([TopicApi], (service) => {
+        service.getTopics().subscribe((response) => {
+          expect(response).toContain('__consumer_offsets');
+        });
+      })();
+    }));
+  });
+});
