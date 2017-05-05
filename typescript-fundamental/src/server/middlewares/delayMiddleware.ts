63cef84bb7fc3ede143ef8bf6cfc028ba5786217
nctuong <wallnguyen@gmail.com>
2017-05-05 22:36:54 +0700

    Use export =

--- typescript-fundamental/src/server/middlewares/delayMiddleware.ts
+++ typescript-fundamental/src/server/middlewares/delayMiddleware.ts
@@ -2,7 +2,9 @@
 /**
  * Created by QuanLe on 5/5/2017.
  */
-export default function (req: any, res: any, next: any) {
+function test(req: any, res: any, next: any) {
     console.log('Hello');
     next();
 }
+
+export = test;
