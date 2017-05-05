d015bcda93e307afd307c4ae58b1bb9488fad14e
Tuong Nguyen <wallnguyen@gmail.com>
2017-05-05 15:54:55 +0700

    Add some tests for ReactiveX

---
+++ Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/kafka-rest/spec/RectiveX.spec.ts
@@ -0,0 +1,64 @@
+import {async} from '@angular/core/testing';
+import {Observable} from 'rxjs/Rx';
+import * as HttpTestErrorHandlers from './HttpTestErrorHandlers';
+/**
+ * Created by nctuong on 5/4/2017.
+ */
+
+describe('ReactiveX', () => {
+
+  describe('#map', () => {
+    it('only handle items in response', (done) => {
+      Observable.throw('error').map(item => 'mapper').subscribe(
+        item => {
+          fail();
+          done();
+        },
+        error => {
+          expect(error).toBe('error');
+          done();
+        }
+      );
+    });
+  });
+
+  describe('#catch', () => {
+    it('convert error into items for response', (done) => {
+      Observable.throw('error').catch(error => {
+        return Observable.of('handledError');
+      }).subscribe(
+          response => {
+            expect(response).toBe('handledError');
+            done();
+          },
+        HttpTestErrorHandlers.failOnError
+        );
+    });
+
+    it('forward items to subscriber', ((done) => {
+      Observable.of('item').catch(error => Observable.of('error'))
+        .subscribe(
+          response => {
+            expect(response).toBe('item');
+            done();
+          },
+          HttpTestErrorHandlers.failOnError
+        );
+    }));
+
+    it('forward items to subscriber and transform error before forward to subscriber', (done) => {
+      Observable.of('item').concat(Observable.throw('test'))
+        .catch(error => Observable.of(''))
+        .subscribe(
+          (item) => {
+            expect(['item', '']).toContain(item);
+            done();
+          },
+          error => {
+            fail();
+            done();
+          }
+        );
+    });
+  });
+});
