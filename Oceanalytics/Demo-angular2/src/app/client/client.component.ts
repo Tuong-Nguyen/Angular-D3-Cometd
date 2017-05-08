9ec27bf5903fa92311299d9d9ad0ba6150ae5320
trinhviethanh2007 <trinhviethanh2007@gmail.com>
2017-05-08 17:18:06 +0700

    integrate server

--- Oceanalytics/Demo-angular2/src/app/client/client.component.ts
+++ Oceanalytics/Demo-angular2/src/app/client/client.component.ts
@@ -1,15 +1,113 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from './client.service';
+import { DatePipe } from '@angular/common';
+import {SelectModule} from 'angular2-select';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.css']
+  styleUrls: ['./client.component.css'],
+  providers: [ClientService]
 })
 export class ClientComponent implements OnInit {
+  public isReady = false;
+  public newInstance;
+  public urlInstance;
+  public status = 'Create Instance';
+  public datePipe= new DatePipe('en-US');
+  public currentDate = this.datePipe.transform(new Date(), 'HHmmss');
+  public groupName = 'Oceana_' + this.currentDate;
+  public instanceName = 'Instance_' + this.currentDate;
+  public input = {
+    'records': [
+      {
+        'key': this.instanceName,
+        'value': {
+          'kafkaTopicName': 'oceanalytics-realtime-agentmeasures',
+          'subscriptionRequest': {
+            'measuresStream': 'AgentPerformanceRealTimeMeasures',
+            'password': 'secret',
+            'user': this.instanceName
+          }
+        }
+      }
+    ]
+  };
 
-  constructor() { }
+  public pump = {
+    'records': [
+      {
+        'key': this.instanceName,
+        'value': {
+          'pump': this.instanceName
+        }
+      }
+    ]
+  };
+
+  constructor(private _clientService: ClientService) { }
 
   ngOnInit() {
+    this.createInstance();
   }
 
+  createInstance(): any {
+    this.isReady = false;
+    const instance = {
+      'name': this.instanceName,
+      'format': 'json',
+      'auto.offset.reset': 'latest', // earliest
+      'auto.commit.enable': 'false'
+    };
+
+    this._clientService.createInstance(this.groupName, instance).subscribe(
+      data => {
+        this.newInstance = data;
+        console.log(data);
+
+        // Change urlInstance
+        this.urlInstance = data.base_uri;
+      },
+      err => {
+        console.log('====Create Instance Fail======');
+        console.log(err);
+      },
+      () => this.subscribeTopic()
+    );
+  }
+
+  subscribeTopic(): any {
+    const listTopics = {
+      'topics': [
+        'RSR',
+        'pump'
+      ]
+    };
+    // Change status
+    this.status = 'Subscribe';
+    // Call Service
+    this._clientService.subscribeTopic(this.urlInstance, listTopics).subscribe(
+      data => {
+        console.log('=====Subscription Success=====');
+        this.isReady = true;
+        this.status = 'Listening';
+      },
+      err => {
+        console.log('=====Subscription Fail=====');
+      }
+    );
+  }
+
+
+  addRecord(): any {
+
+    this._clientService.addRecord('RSR', this.input).subscribe(
+      data => {
+        console.log('====== Push data into RSR topic ====', data);
+      },
+      err => {
+        console.log('===== Push data into RSR topic unsuccessfully =====', err);
+      }
+    );
+  }
 }
--- Oceanalytics/Demo-angular2/src/app/client/client.service.ts
+++ Oceanalytics/Demo-angular2/src/app/client/client.service.ts
@@ -1,8 +1,66 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class ClientService {
+  constructor(private http: Http) { };
 
-  constructor() { }
+  /**
+   * @Description: create a new instance
+   * @param groupName
+   * @param data
+   */
+  createInstance(groupName, data): Observable<any> {
+    const apiUrl = environment.server + '/consumers/' + groupName;
+    const headers = new Headers({ 'Content-Type': 'application/vnd.kafka.json.v2+json', 'Accept' : 'application/vnd.kafka.v2+json'});
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(apiUrl, data, options).map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  /**
+   * @Description subcribe a topic
+   * @param urlInstance
+   * @param data
+   * @returns {Observable<R|T>}
+   */
+  subscribeTopic(urlInstance, data): Observable<any> {
+    const apiUrl = urlInstance + '/subscription';
+    const headers = new Headers({ 'Content-Type': 'application/vnd.kafka.v2+json'});
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(apiUrl, data, options).map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || environment.generalErr));
+  }
+
+  /**
+   * @Description get record from topics
+   * @param urlInstance
+   * @returns {Observable<R|T>}
+   */
+  getRecord(urlInstance): Observable<any> {
+    const apiUrl = urlInstance + '/records';
+    const headers = new Headers({ 'Accept': 'application/vnd.kafka.json.v2+json'});
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(apiUrl, options).map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json() || environment.generalErr));
+  }
+
+  addRecord(topic, data): Observable<any> {
+    const apiUrl = environment.server + '/topics/' + topic;
+    const headers = new Headers({ 'Content-Type': 'application/vnd.kafka.json.v2+json', 'Accept': 'application/vnd.kafka.v2+json, application/vnd.kafka+json, application/json'});
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(apiUrl, data, options).map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json() || environment.generalErr));
+  }
 
 }
