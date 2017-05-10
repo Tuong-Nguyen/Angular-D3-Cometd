1e8338ce5727e98b31383dbd4192942a919d1515
Trinh Viet Hanh <tvhanh@TMA.COM.VN>
2017-05-10 10:29:43 +0700

    integrate multiple topics

--- Oceanalytics/Demo-angular2/src/app/server/server.component.ts
+++ Oceanalytics/Demo-angular2/src/app/server/server.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, OnChanges, Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import {Component, OnInit, OnChanges, Pipe, PipeTransform} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
-import { Observable } from 'rxjs/Rx';
+import {Observable} from 'rxjs/Rx';
 
-import { Consumer } from 'app/server/consumer';
+import {Consumer} from 'app/server/consumer';
 
-import { Record } from 'app/server/record';
+import {Record} from 'app/server/record';
 
-import { ServerService } from 'app/server/server.service';
+import {ServerService} from 'app/server/server.service';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-server',
@@ -16,230 +17,235 @@ import { ServerService } from 'app/server/server.service';
 })
 
 export class ServerComponent implements OnInit, OnChanges {
-  
+
   newInstance: Consumer;
 
-  constructor(private _serverService: ServerService) { }
-  public datePipe= new DatePipe('en-US');
-  public currentDate = this.datePipe.transform(new Date(), "HHmmss");
+  constructor(private _serverService: ServerService) {
+  }
 
-  public groupName = 'Oceana_'+ this.currentDate;
+  public datePipe = new DatePipe('en-US');
+  public currentDate = this.datePipe.transform(new Date(), 'HHmmss');
+
+  public groupName = 'Oceana_' + this.currentDate;
   public instanceName = 'Instance_' + this.currentDate;
 
   public urlInstance = '';
-  public status = "Create Instance";
+  public status = 'Create Instance';
 
   public records = [];
 
-  public isReady : Boolean = false ;
-  public isDisplay : Boolean = false;
-  public flag : Boolean = true;
-  public isPump : Boolean = false;
-  public isPending : Boolean = false;
+  public isReady: Boolean = false;
+  public isDisplay: Boolean = false;
+  public flag: Boolean = true;
+  public isPump: Boolean = false;
+  public isPending: Boolean = false;
 
 
-  public dtrs : any;
-  public tpName : string;
-  public arrTopicName : Array<{name: string}> = [];
-
+  public dtrs: any;
+  public tpName: string;
+  public arrTopicName = [];
+  public arrLabelName = [];
   private timer;
 
 
+  createInstance(): any {
+    this.isDisplay = false;
+    let data = {
+      'name': this.instanceName,
+      'format': 'json',
+      'auto.offset.reset': 'latest', // earliest
+      'auto.commit.enable': 'false'
+    };
+    // Call Service
+    this._serverService.createInstance(this.groupName, data).subscribe(
+      data => {
+        this.newInstance = data;
+        console.log('====Create Instance Success======');
+        console.log(data);
 
-
-  createInstance(): any{
-  	this.isDisplay = false;
-  	let data = {
-		  "name": this.instanceName,
-		  "format": "json",
-		  "auto.offset.reset": "latest", //earliest
-		  "auto.commit.enable": "false"
-		};
-	//Call Service
-  	this._serverService.createInstance(this.groupName, data).subscribe(
-	  		data => {
-	  			this.newInstance = data;
-	  			console.log("====Create Instance Success======");
-	  			console.log(data);
-
-	  			//Change urlInstance
-	  			this.urlInstance = data.base_uri;
-	  		},
-	  		err => {
-	  			console.log("====Create Instance Fail======");
-	  			console.log(err);
-  			},
-  			() => this.subscribeTopic()
-  		);
+        // Change urlInstance
+        this.urlInstance = data.base_uri;
+      },
+      err => {
+        console.log('====Create Instance Fail======');
+        console.log(err);
+      },
+      () => this.subscribeTopic()
+    );
   }
 
-  subscribeTopic(): any{
-  	let listTopics = {
-	  		'topics': [
-	  			"RSR",
-	  			"pump"
-  			]
-	  	}
-	//Change status
-	this.status = "Subscribe";
-	//Call Service
-  	this._serverService.subscribe(this.urlInstance, listTopics).subscribe(
-  			data => {
-  				console.log("=====Subscription Success=====");
-  				this.isReady = true;
-  				this.status = "Listening";
-  			},
-  			err =>{
-  				console.log("=====Subscription Fail=====");
-  			},
-  			() => this.fetchData()
-  		);
+  subscribeTopic(): any {
+    const listTopics = {
+      'topics': [
+        environment.rsr,
+        environment.pump
+      ]
+    }
+    // Change status
+    this.status = 'Subscribe';
+    // Call Service
+    this._serverService.subscribe(this.urlInstance, listTopics).subscribe(
+      data => {
+        console.log('=====Subscription Success=====');
+        this.isReady = true;
+        this.status = 'Listening';
+      },
+      err => {
+        console.log('=====Subscription Fail=====');
+      },
+      () => this.fetchData()
+    );
   }
 
-  fetchData(): any{
-  	//Code here
- 	if (this.isReady === true && this.urlInstance != '' && this.flag === true) {
- 		this.timer = setInterval(()=>{
- 			if (!this.isPending) {
- 				this.isPending =true;
- 				let arrayUrl : any[];
- 				this.isDisplay = true;
- 				//Call service
-				this._serverService.getRecords(this.urlInstance).subscribe(
-					data => {
-						console.log("===Get records success===");
-						console.log("datttttttttttaaaaaaaaaaa");
-						console.log(data);
-						var topicName = '';
-						var dataTmp1 : any;
-						var dataTmp2 : any;
+  fetchData(): any {
+    // Code here
+    if (this.isReady === true && this.urlInstance != '' && this.flag === true) {
+      this.timer = setInterval(() => {
+        if (!this.isPending) {
+          this.isPending = true;
+          let arrayUrl: any[];
+          this.isDisplay = true;
+          // Call service
+          this._serverService.getRecords(this.urlInstance).subscribe(
+            data => {
+              console.log('===Get records success===');
+              console.log(data);
+              let topicName = '';
+              let dataTmp1: any;
+              let dataTmp2: any;
 
-						this.isPending = false;
-						this.isDisplay = true;
-						
-						if (data.length > 0) {
-							this.records = this.records.concat(data);
-							for (var i = 0; i < data.length; i++) {
-								if (data[i].value.pump === undefined) {
-									this.status = "Listening Pump";
-									this.flag = false;
-									// this.tpName = topicName = data[i].value.subscriptionRequest.measuresStream;
-									topicName = data[i].value.subscriptionRequest.measuresStream;
+              this.isPending = false;
+              this.isDisplay = true;
 
-									console.log('topicName');
-									console.log(topicName);
+              if (data.length > 0) {
+                this.records = this.records.concat(data);
+                console.log(data);
+                // this.arrTopicName = [];
+                // this.arrLabelName = [];
+                for (let i = 0; i < data.length; i++) {
+                  if (data[i].value.pump === undefined) {
+                    this.status = 'Listening Pump';
+                    this.flag = false;
+                    // this.tpName = topicName = data[i].value.subscriptionRequest.measuresStream;
+                    topicName = data[i].value.subscriptionRequest.measuresStream;
 
-									
-									this.arrTopicName.push(data[i].value.subscriptionRequest.measuresStream);
+                    console.log('topicName');
+                    console.log(topicName);
 
-									console.log('arrTopicName');
-									console.log(this.arrTopicName);
 
-									this.dtrs = dataTmp1 = {
-										"records": [
-											{
-												"key": data[i].key,
-												"value": data[i].value
-											}
-										]
-									}
+                    this.arrTopicName.push(data[i].value.subscriptionRequest.measuresStream);
+                    this.arrLabelName.push(data[i].value.kafkaTopicName);
+                    // console.log('arrTopicName');
+                    // console.log(this.arrTopicName);
 
-									this._serverService.addRecord(topicName, dataTmp1).subscribe(
-										res1 => {
-											console.log("===Add new message to topic===");
-					                        console.log(res1);
-										},
-										err1 => {
-											console.log("===Add message fail 1===");
-										}
-									)
+                    this.dtrs = dataTmp1 = {
+                      'records': [
+                        {
+                          'key': data[i].key,
+                          'value': data[i].value
+                        }
+                      ]
+                    }
 
-									//push data to result topic
-									dataTmp2 = {
-										"records": [
-											{
-												"key": data[i].key,
-												"value": topicName
-											}
-										]
-									}
+                    this._serverService.addRecord(topicName, dataTmp1).subscribe(
+                      res1 => {
+                        console.log('===Add new message to topic===');
+                        console.log(res1);
+                      },
+                      err1 => {
+                        console.log('===Add message fail 1===');
+                      }
+                    )
 
-									this._serverService.addRecord('result', dataTmp2).subscribe(
-										res2 => {
-											console.log("===Add new message to topic 'result' ===");
-											console.log(res2);
-										},
-										err2 => {
-											console.log("===Add message fail 2===");
-										}
-									)
+                    // push data to result topic
+                    dataTmp2 = {
+                      'records': [
+                        {
+                          'key': data[i].key,
+                          'value': topicName
+                        }
+                      ]
+                    }
+                    console.log(dataTmp2);
+                    this._serverService.addRecord('result', dataTmp2).subscribe(
+                      res2 => {
+                        console.log('===Add new message to topic result ===')
+                        ;
+                        console.log(res2);
+                      },
+                      err2 => {
+                        console.log('===Add message fail 2===');
+                      }
+                    )
 
-								}
-								else {
-									this.status = "Ready";
-									this.isPump = true;
-								}
-							}
-						}
-						else {
-							this.status = "Sending";
-							console.log("Case else");
-							console.log("is pump: "+ this.isPump);
-							console.log("Topic Name: " + this.tpName);
-							console.log("Arr topic: ");
-							console.log(this.arrTopicName);
+                  }
+                  else {
+                    this.status = 'Ready';
+                    this.isPump = true;
+                  }
+                }
+              } else {
+                this.status = 'Sending';
+                console.log('Case else');
+                console.log('is pump: ' + this.isPump);
+                console.log('Arr topic: ');
+                console.log(this.arrTopicName);
+                console.log('Arr label: ');
+                console.log(this.arrLabelName);
 
-							console.log(this.dtrs);
 
-							if (this.dtrs != '' && this.dtrs !== undefined && this.isPump == true) {
-			            		this.dtrs.records[0].value.time = this.datePipe.transform(new Date(), "HHmmss");
+                if (this.dtrs != '' && this.dtrs !== undefined && this.isPump == true) {
+                  console.log(this.dtrs);
+                  var dttime = this.datePipe.transform(new Date(), 'HHmmss');
 
-								for (var i = 0; i < this.arrTopicName.length; i++) {
-									this._serverService.addRecord(this.arrTopicName[i], this.dtrs).subscribe(
-				            			res1 => {
-				            				console.log("===Add new message to topic===");
-				            				console.log(res1);
-				            			},
-				            			err1 => {
-				            				console.log("===Add message fail 3===");
-				            			}
-									)
-								}
-							}
-						}
+                  for (var i = 0; i < this.arrTopicName.length; i++) {
+                    this.dtrs.records[0].value.kafkaTopicName = this.arrLabelName[i];
+                    this.dtrs.records[0].value.time = dttime;
+                    this._serverService.addRecord(this.arrTopicName[i], this.dtrs).subscribe(
+                      res1 => {
+                        console.log('===Add new message to topic===');
+                        console.log(res1);
+                      },
+                      err1 => {
+                        console.log('===Add message fail 3===');
+                      }
+                    )
+                  }
+                }
+              }
 
-					},
-					err => {
-						console.log("===Get records fail===");
-					}
-				)
- 			}
- 		}, 2000);
- 	}
+            },
+            err => {
+              console.log('===Get records fail===');
+            }
+          )
+        }
+      }, 2000);
+    }
   }
 
-  deleteInstance(): any{
-  	this._serverService.deleteInstance(this.urlInstance).subscribe(
-  		data => {
-  			console.log("Delete ins" + data);
-  			this.records = [];
-  			clearInterval(this.timer);
-  			this.isReady = false;
-  		},
-  		err =>{
-  			console.log("=====Delete Ins Fail=====");
-  		}
-  	);
+  deleteInstance(): any {
+    this._serverService.deleteInstance(this.urlInstance).subscribe(
+      data => {
+        console.log('Delete ins' + data);
+        this.records = [];
+        clearInterval(this.timer);
+        this.isReady = false;
+      },
+      err => {
+        console.log('=====Delete Ins Fail=====');
+      }
+    );
   }
 
 
   ngOnInit(): void {
- 	this.createInstance();
+    this.createInstance();
 
- 	
+
   }
 
-  ngOnChanges(){
+  ngOnChanges() {
 
   }
 
