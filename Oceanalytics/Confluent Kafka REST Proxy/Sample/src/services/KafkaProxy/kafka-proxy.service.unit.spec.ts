5de1d5fa54a777630429e05725cf70347e8cc60c
Tuong Nguyen <wallnguyen@gmail.com>
2017-05-11 09:31:24 +0700

    Group test of subscribeTopics into a group

--- Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/KafkaProxy/kafka-proxy.service.unit.spec.ts
+++ Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/KafkaProxy/kafka-proxy.service.unit.spec.ts
@@ -51,42 +51,44 @@ describe('KafkaProxyService - UnitTest', () => {
       expect(service.instanceId).toBe('id');
     });
 
-    it('#subscribeTopics will create the consumer instance automatically if it does not exist', () => {
-      const mockResponse = new Response({}, {status: 404});
-      // error on first call and success on the second
-      spyOn(mockConsumerApi, 'subscribesTopics').and.callFake(() => {
-        if ((mockConsumerApi.subscribesTopics as any).calls.count() === 1) {
-          return Observable.throw(mockResponse);
-        } else {
-          return Observable.from([{}]);
-        }
-      });
-      spyOn(mockConsumerApi, 'createInstanceToGroup').and.returnValue(Observable.from([{
-        instance_id: 'id',
-        base_uri: 'base_uri'
-      }]));
-
-      service.subscribeTopics()
-        .subscribe();
-
-      expect(mockConsumerApi.createInstanceToGroup).toHaveBeenCalled();
-      expect(service.instanceId).toBe('id');
-    });
-
-    it('#subscribeTopics throws error if it is not non-existing consumer instance error', () => {
-      const mockResponse = new Response({}, {status: 500});
-      // error on first call and success on the second
-      spyOn(mockConsumerApi, 'subscribesTopics').and.returnValue(Observable.throw(mockResponse));
-
-      service.subscribeTopics()
-        .subscribe(
-          item => {
-            fail();
-          },
-          error => {
-            expect(error.status).toBe(500);
+    describe('#subscribeTopics', () => {
+      it('will create the consumer instance automatically if it does not exist', () => {
+        const mockResponse = new Response({}, {status: 404});
+        // error on first call and success on the second
+        spyOn(mockConsumerApi, 'subscribesTopics').and.callFake(() => {
+          if ((mockConsumerApi.subscribesTopics as any).calls.count() === 1) {
+            return Observable.throw(mockResponse);
+          } else {
+            return Observable.from([{}]);
           }
-        );
+        });
+        spyOn(mockConsumerApi, 'createInstanceToGroup').and.returnValue(Observable.from([{
+          instance_id: 'id',
+          base_uri: 'base_uri'
+        }]));
+
+        service.subscribeTopics()
+          .subscribe();
+
+        expect(mockConsumerApi.createInstanceToGroup).toHaveBeenCalled();
+        expect(service.instanceId).toBe('id');
+      });
+
+      it('throws error if it is not non-existing consumer instance error', () => {
+        const mockResponse = new Response({}, {status: 500});
+        // error on first call and success on the second
+        spyOn(mockConsumerApi, 'subscribesTopics').and.returnValue(Observable.throw(mockResponse));
+
+        service.subscribeTopics()
+          .subscribe(
+            item => {
+              fail();
+            },
+            error => {
+              expect(error.status).toBe(500);
+            }
+          );
+      });
     });
   });
 });
