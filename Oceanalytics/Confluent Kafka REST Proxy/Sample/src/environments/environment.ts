5be59df60a6a0220f99d8d45398e4ce23fe8b2b6
nctuong <wallnguyen@gmail.com>
2017-05-13 05:02:32 +0700

    Define kafka_rest_proxy information

--- Oceanalytics/Confluent Kafka REST Proxy/Sample/src/environments/environment.ts
+++ Oceanalytics/Confluent Kafka REST Proxy/Sample/src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  kafka_rest_proxy: 'http://192.168.227.132:8082'
 };
