518b30b643285c7f4f56cfeed2285077be9e9f8a
nctuong <wallnguyen@gmail.com>
2017-05-13 05:03:47 +0700

    Use Environment information

--- Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/kafka-rest/spec/SystemInfo.ts
+++ Oceanalytics/Confluent Kafka REST Proxy/Sample/src/services/kafka-rest/spec/SystemInfo.ts
@@ -1,7 +1,8 @@
+import {environment} from '../../../environments/environment';
 /**
  * Created by nctuong on 5/5/2017.
  */
 const topicName = 'TestTopic';
-const kafka_rest_server = 'http://11.11.254.102:8082';
+const kafka_rest_server = environment.kafka_rest_proxy;
 
 export {topicName, kafka_rest_server};
