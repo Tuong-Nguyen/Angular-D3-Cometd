1c8eccf7865a5995d161608970a2d2f680c2271f
Tuong Nguyen <wallnguyen@gmail.com>
2017-05-24 13:15:53 +0700

    fix tslint error

--- Oceanalytics/Demo-angular2/src/app/tabular-table/tabular-table.component.ts
+++ Oceanalytics/Demo-angular2/src/app/tabular-table/tabular-table.component.ts
@@ -2,37 +2,38 @@ import {Component, Input, OnInit} from '@angular/core';
 import {environment as env} from '../../environments/environment';
 
 @Component({
-	selector: 'app-tabular-table',
-	templateUrl: './tabular-table.component.html',
-	styleUrls: ['./tabular-table.component.css']
+  selector: 'app-tabular-table',
+  templateUrl: './tabular-table.component.html',
+  styleUrls: ['./tabular-table.component.css']
 })
-export class TabularTableComponent implements OnInit  {
+export class TabularTableComponent implements OnInit {
 
-	@Input() public data: Array<any>;
-	@Input() public listTopicProperties: Array<any>;
+  @Input() public data: Array<any>;
+  @Input() public listTopicProperties: Array<any>;
 
-	public fields : Array<any> = [];
+  public fields: Array<any> = [];
 
-	public env = env
+  public env = env;
 
-	public showTab = true;
+  public showTab = true;
 
-	constructor() { }
+  constructor() {
+  }
 
-	ngOnInit() {
-	}
+  ngOnInit() {
+  }
 
-	public checkItem(item): any{
-		const index = this.fields.indexOf(item);
-		console.log('index: ', index);
-		if (index > -1) {
-			this.fields.splice(index, 1);
-		} else {
-			this.fields.push(item);
-		}
-	}
+  public checkItem(item): any {
+    const index = this.fields.indexOf(item);
+    console.log('index: ', index);
+    if (index > -1) {
+      this.fields.splice(index, 1);
+    } else {
+      this.fields.push(item);
+    }
+  }
 
-	public displayTab(showTab): void{
-		this.showTab = !showTab;
-	}
+  public displayTab(showTab): void {
+    this.showTab = !showTab;
+  }
 }
