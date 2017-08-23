1e3784a4e6cb6f54e515131978a8fa4700d4761b
Le Ngoc Thao <lnthao@tma.com.vn>
2017-08-23 17:51:34 +0700

    Use @ViewChild to get form reference

--- reporting-widgets/src/app/form-example/form-example.component.ts
+++ reporting-widgets/src/app/form-example/form-example.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form-example',
@@ -6,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-example.component.css']
 })
 export class FormExampleComponent {
+  @ViewChild('f') aForm: NgForm;
   secrectQuestions = [
     {value: 0, viewValue: 'Your first Pet?'},
     {value: 1, viewValue: 'Who is your best friend?'},
     {value: 2, viewValue: 'What is first birthday gift?'}
   ];
 
-  onSubmit(form: HTMLFormElement) {
-    console.log(form);
+  // onSubmit(form: HTMLFormElement) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    console.log(this.aForm);
   }
 }
